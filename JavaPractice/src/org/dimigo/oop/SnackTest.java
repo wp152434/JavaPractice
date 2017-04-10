18fef419bbad1234a693c6f31400f97edf6c6a48
choichanhwan <ghks9791@naver.com>
2017-04-10 09:10:47 +0900

    Update SnackTest.java
    문제에 맞춤

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -2,12 +2,16 @@ package org.dimigo.oop;
 
 public class SnackTest {
 	public static void main(String[] args){
-		Snack a = new Snack("»õ¿ì±ø", "³ó½É", 1100, 2);
-		Snack b = new Snack("ÄÜ¯…","Å©¶ó¿î", 1200, 1);
-		Snack c = new Snack("Çã´Ï¹öÅÍÄ¨","ÇØÅÂ", 1500, 4);
+		Snack[] a = new Snack[3];
+		int sum = 0;
+		Snack a[0] = new Snack("è´§å¿«å…µ", "ä¸‘ç¼´", 1100, 2);
+		Snack a[1] = new Snack("èƒ½ç˜","å†œæ‰¼æ¬¾", 1200, 1);
+		Snack a[2] = new Snack("å€¾èªæ»šç£æŠ¹","ç§¦æ€•", 1500, 4);
 		
-		System.out.println(a.toString());
-		System.out.println(b.toString());
-		System.out.println(c.toString());
+		for(int i=0;i<3;i++){
+		    System.out.println(a[i].toString());
+		    sum += a[i].calcPrice();
+		}
+		System.out.println(sum);
 	}
 }
