a74a209f4f298848a6ec945d9e07688db2c1c2d1
DESKTOP-UNK0V5I\ghks9 <ghks9791@naver.com>
2017-04-10 09:50:56 +0900

    no message

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -2,11 +2,12 @@ package org.dimigo.oop;
 
 public class SnackTest {
 	public static void main(String[] args){
-		Snack[] a = new Snack[3];
+		Snack[] a = null;
+		a = new Snack[3];
 		int sum = 0;
-		Snack a[0] = new Snack("货快兵", "丑缴", 1100, 2);
-		Snack a[1] = new Snack("能瘏","农扼款", 1200, 1);
-		Snack a[2] = new Snack("倾聪滚磐抹","秦怕", 1500, 4);
+		a[0] = new Snack("兀㎩엮�뀿", "訝묊섦", 1100, 2);
+		a[1] = new Snack("�꺗�삈","�넑�돹轝�", 1200, 1);
+		a[2] = new Snack("�얕겒譯싩즾�듅","燁���", 1500, 4);
 		
 		for(int i=0;i<3;i++){
 		    System.out.println(a[i].toString());
