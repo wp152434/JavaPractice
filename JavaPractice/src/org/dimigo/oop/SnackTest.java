063f1100846a589ee5872bf415f97fef665b8fe8
choichanhwan <ghks9791@naver.com>
2017-04-18 16:38:58 +0900

    Update SnackTest.java

--- JavaPractice/src/org/dimigo/oop/SnackTest.java
+++ JavaPractice/src/org/dimigo/oop/SnackTest.java
@@ -5,9 +5,9 @@ public class SnackTest {
 		Snack[] a = null;
 		a = new Snack[3];
 		int sum = 0;
-		a[0] = new Snack("兀㎩엮�뀿", "訝묊섦", 1100, 2);
-		a[1] = new Snack("�꺗�삈","�넑�돹轝�", 1200, 1);
-		a[2] = new Snack("�얕겒譯싩즾�듅","燁���", 1500, 4);
+		a[0] = new Snack("새우깡", "농심", 1100, 2);
+		a[1] = new Snack("콘칩","크라운", 1200, 1);
+		a[2] = new Snack("허니버터칩","", 1500, 4);
 		
 		for(int i=0;i<3;i++){
 		    System.out.println(a[i].toString());
